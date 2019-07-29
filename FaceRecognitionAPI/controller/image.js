54c5af5afda8c9491c8149cece4aa8101530755f
ipswebdev <sawant.pranay.15ee1102@gmail.com>
2019-07-29 21:24:48 +0530

    all files added for the smart brain API

---
+++ FaceRecognitionAPI/controller/image.js
@@ -0,0 +1,28 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+ apiKey: 'b34b1910d79841c998d0f0ef60195d66'
+});
+
+const  handleApiCall = (req,res) =>{
+app.models
+.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.then(data =>{
+	res.json(data);
+})
+.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req,res,db)=>{
+	const {id}=req.body;
+db('users').where('id','=',id)
+.increment('entries',1)
+.returning('entries')
+.then(entries => {
+res.json(entries[0]);
+})
+.catch(err=>res.status(400).json('unable to update'))
+}
+module.exports={
+	handleImage,
+	handleApiCall
+}
